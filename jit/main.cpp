2431d158c94326be120142f975391e03616d3199
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2020-10-13 13:58:00 -0700

    add JIT setup

--- jit/main.cpp
+++ jit/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+
+#include "parser.h"
 
 int main()
 {
-	std::cout << "Hello world!" << '\n';
+	std::wstring source{ L"def plustwo(x) x + 2;" };
+	std::wstringstream sourceStream{ source };
+	
+	lexer::Lexer lexer{ sourceStream };
+
+	parser::Parser parser{};
+	auto ast = parser.parse(lexer);
 
 	return 0;
 }
