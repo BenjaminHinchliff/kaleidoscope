9160fc77d5eb3ed9797536864ef2bbcf3237a9ca
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2020-10-25 16:05:37 -0700

    make casts more explicit and tweak variable naming

--- jit/jit.cpp
+++ jit/jit.cpp
@@ -11,10 +11,10 @@
 template <class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template <class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
-using jit_ptr = std::unique_ptr<llvm::orc::KaleidoscopeJIT>;
+using jit_ptr_t = std::unique_ptr<llvm::orc::KaleidoscopeJIT>;
 
 std::unique_ptr<llvm::Module> makeModule(llvm::LLVMContext &context,
-                                         const jit_ptr &jit) {
+                                         const jit_ptr_t &jit) {
   auto llvmMod = std::make_unique<llvm::Module>("KaleidoscopeJIT", context);
   llvmMod->setDataLayout(jit->getTargetMachine().createDataLayout());
   return llvmMod;
@@ -26,7 +26,7 @@ void parseAndExecuteTokenStream(lexer::Lexer &lexer,
                                 llvm::IRBuilder<> &builder,
                                 ast::named_values_t &namedValues,
                                 ast::function_protos_t &functionProtos,
-                                jit_ptr &jit) {
+                                jit_ptr_t &jit) {
   auto llvmModule = makeModule(context, jit);
 
   while (!std::holds_alternative<tokens::Eof>(lexer.peek())) {
@@ -49,8 +49,8 @@ void parseAndExecuteTokenStream(lexer::Lexer &lexer,
 
     auto exprSymbol = jit->findSymbol("__anon_expr");
     if (exprSymbol) {
-      double (*fP)() =
-          (double (*)())(intptr_t)llvm::cantFail(exprSymbol.getAddress());
+      double (*fP)() = reinterpret_cast<double (*)()>(
+          static_cast<intptr_t>(llvm::cantFail(exprSymbol.getAddress())));
       std::cout << "Eval:\n" << fP() << '\n';
       jit->removeModule(modHandle);
     }
